e238cda47c3409be16c78e42412f5f8878d63eb8
MikeKibz59 <mikekibz59@gmail.com>
2026-01-04 03:05:06 +0300

    added tailwindcss and navbar skeleton

---
+++ techpack_op_frontend/src/components/Navbar.vue
@@ -0,0 +1,8 @@
+<template>
+    <nav class="flex flex-row justify-between align-middle leading-normal pt-4 pb-4 pr-6 pl-6 bg-[#FAF9F5]">
+        <div class=" bg-[#E0E079] text-[#E17B79] text-2xl font-extrabold italic color">TPK</div>
+        <ul class="font-normal opacity-60">
+            <li>Your uploads</li>
+        </ul>
+    </nav>
+</template>
